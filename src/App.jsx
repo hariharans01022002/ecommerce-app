23cb7def54551105c947261a64741381a375cfc3
Hariharan <hariharans01022002@gmail.com>
2026-04-07 20:32:14 +0530

    Update | Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import ProductList from './pages/ProductList'
 import ProductDetails from './pages/ProductDetails'
 import CartPage from './pages/CartPage'
@@ -10,26 +9,24 @@ import OrdersPage from "./pages/OrdersPage"
 import OrderDetails from "./pages/OrderDetails"
 import {useAuth} from "./Context/AuthContext"
 import Login from "./pages/Login"
+import Layout from './components/Layout'
 
 const App = () => {
   const { user } = useAuth()
 
-  if(!user) return <Login />
-
   return (
-    <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={ <ProductList />} />
-        <Route path="/product/:id" element={ <ProductDetails />} />
-        <Route path="/cart" element={ <CartPage /> } />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/success" element={<SuccessPage />} />
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/orders/:id" element={<OrderDetails />} />
+        <Route path ="/login" element={<Login />} />
+        <Route path = "/" element = {user ? <Layout /> : <Navigate to="/login" />} >
+          <Route path="/" element={ <ProductList />} />
+          <Route path="/product/:id" element={ <ProductDetails />} />
+          <Route path="/cart" element={ <CartPage /> } />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/orders/:id" element={<OrderDetails />} />
+        </Route>
       </Routes>
-     
-    </>
   )
 }
 
